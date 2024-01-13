b3bc30409f6924ad8d34e363e7ad7ae6bc26cdc5
alperengokbak <swalperen3008@gmail.com>
2024-01-13 04:40:16 +0300

    Added CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(
   "/payment-service",
